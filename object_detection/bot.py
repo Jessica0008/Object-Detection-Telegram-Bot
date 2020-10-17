e799ba1df9c2e71035a95d353269f23d0e813153
gorl <marina.zmieva@gmail.com>
2020-10-17 15:19:11 +0300

    picture download (#12)
    * picture download
    
    * image saving with tokens and chat id
    
    * add empty count_cars
    
    * import count_cars
    
    Co-authored-by: Gleb Sinyakov <sinyakov@phystech.edu>

--- object_detection/bot.py
+++ object_detection/bot.py
@@ -1,20 +1,17 @@
 from settings import API_KEY, PROXY_PASSWORD, PROXY_URL, PROXY_USERNAME
-from handlers import give_menu, count_cars, detect_defects
+from handlers import detect_defects, count_cars
+from utils import give_menu, send_picture, get_picture
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup
 import logging
 from glob import glob
-from random import randint, choice  
+from random import randint, choice
 
 
-
-PROXY = {'proxy_url': PROXY_URL,
-        'urllib3_proxy_kwargs': {
-        'username': PROXY_USERNAME,
-        'password': PROXY_PASSWORD
-    }
+PROXY = {
+    "proxy_url": PROXY_URL,
+    "urllib3_proxy_kwargs": {"username": PROXY_USERNAME, "password": PROXY_PASSWORD},
 }
-
 
 
 def main():
@@ -22,21 +19,18 @@ def main():
     bot = Updater(API_KEY, use_context=True)
 
     dp = bot.dispatcher
-    dp.add_handler(CommandHandler("detect_defects", detect_defects))
-    dp.add_handler(CommandHandler("count_cars", count_cars))
-    
-    dp.add_handler(MessageHandler(Filters.photo, give_menu))
-    dp.add_handler(MessageHandler(Filters.regex('^(Count Cars)$'), count_cars))
-    dp.add_handler(MessageHandler(Filters.regex('^(Detect Defects)$'), detect_defects))
-    print("starting")
+
+    # dp.add_handler(CommandHandler("start", give_menu))
+
+    dp.add_handler(MessageHandler(Filters.photo, get_picture))
+    # dp.add_handler(MessageHandler(Filters.regex("^(Count Cars)$"), count_cars))
+    dp.add_handler(MessageHandler(Filters.regex("^(Detect Defects)$"), detect_defects))
+
     # Командуем боту начать ходить в Telegram за сообщениями
     bot.start_polling()
     # Запускаем бота, он будет работать, пока мы его не остановим принудительно
     bot.idle()
 
 
-
 if __name__ == "__main__":
     main()
-
-
